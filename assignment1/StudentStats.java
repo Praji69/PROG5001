1f57c2be3311ee03ccd065200808eec873b26923
prajwal sharma <sharmaprajwal67@gmail.com>
2025-07-21 13:48:11 +1000

    students stats

---
+++ assignment1/StudentStats.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StudentStats {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Input validation
+        System.out.print("Enter number of marks (N): ");
+        while (!input.hasNextInt()) {
+            System.out.println("Invalid input! Enter an integer.");
+            input.next();
+        }
+        int n = input.nextInt();
+        
+        double[] marks = new double[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter mark " + (i+1) + " (0-30): ");
+            while (true) {
+                if (input.hasNextDouble()) {
+                    double mark = input.nextDouble();
+                    if (mark >= 0 && mark <= 30) {
+                        marks[i] = mark;
+                        break;
+                    }
+                }
+                System.out.print("Invalid! Re-enter mark " + (i+1) + " (0-30): ");
+                input.next();
+            }
+        }
